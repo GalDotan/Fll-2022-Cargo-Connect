e0b10dfa26369382e78e771990f42d1a968c620f
GalDotan <93068735+GalDotan@users.noreply.github.com>
2021-12-06 10:10:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,14 +19,14 @@ from core_funcsions import *
 ev3.speaker.beep(500, 3000)
 
 def First_Run():
-    #Gyro_Stright(1)
-    #Gyro_Turn_Left(28)
-    #Gyro_Stright(2.1)
-    #Gyro_Turn_Right(-33)
-    #Gyro_Stright(2)
-    #LMM.run(1000)
-    #wait(10)
-    #LMM.stop()
+    Gyro_Stright(1)
+    Gyro_Turn_Left(28)
+    Gyro_Stright(2.1)
+    Gyro_Turn_Right(-33)
+    Gyro_Stright(2)
+    LMM.run(1000)
+    wait(10)
+    LMM.stop()
     Gyro_Stright(2)
 
 First_Run()
