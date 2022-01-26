84d27ad949d1fd6e80ac83f935c336b1aef4f265
GalDotan <gldotan@gmail.com>
2022-01-26 13:36:27 +0200

    gyro changes

--- Driving_functions.py
+++ Driving_functions.py
@@ -14,39 +14,38 @@ from Object_creation import *
 def Gyroline_W_acceleration(max_speed, PID_Gyrodistance, Target):
     watch = StopWatch()
     degrees = PID_Gyrodistance*-360
-        while RLM.angle() >= degrees:
-            robot.stop()
-            RLM.reset_angle(0)
-            degrees = PID_Gyrodistance*-360
-            Error = 0
-            Intgral = 0
-            Last_Error = 0
-            Derivative = 0
-            Turn_Rate = 0
-            Nag_Turn_Rate = 0
-            Drive_Speed = V_Speed
-            KP = 0
-            KI = 0
-            KD = 0
-            Kp = 3
-            Ki = 0
-            Kd = 0
-            while RLM.angle() >= degrees:
-                Ct = watch.time()
-                max_v = max_speed / 2 * -1
-                V_Speed = Ct * max_v
-                Error = Target-Gyrogirl.angle()
-                Intgral = Intgral+Error*0.001
-                KI = Intgral*Ki 
-                Derivative = Error-Last_Error 
-                Last_Error = Error
-                KD = Derivative*Kd 
-                Turn_Rate = KP+KD+KI
-                Nag_Turn_Rate = Turn_Rate*1
-                robot.drive(Drive_Speed , Nag_Turn_Rate ) 
-                wait(1)
-            robot.stop()
-            RLM.reset_angle(0)
+    robot.stop()
+    RLM.reset_angle(0)
+    degrees = PID_Gyrodistance*-360
+    Error = 0
+    Intgral = 0
+    Last_Error = 0
+    Derivative = 0
+    Turn_Rate = 0
+    Nag_Turn_Rate = 0
+    Drive_Speed = V_Speed
+    KP = 0  
+    KI = 0
+    KD = 0
+    Kp = 3
+    Ki = 0
+    Kd = 0
+    while RLM.angle() >= degrees:
+        Ct = watch.time()
+        max_v = max_speed / 2 * -1
+        V_Speed = Ct * max_v
+        Error = Target-Gyrogirl.angle()
+        Intgral = Intgral+Error*0.001
+        KI = Intgral*Ki 
+        Derivative = Error-Last_Error 
+        Last_Error = Error
+        KD = Derivative*Kd 
+        Turn_Rate = KP+KD+KI
+        Nag_Turn_Rate = Turn_Rate*1
+        robot.drive(Drive_Speed , Nag_Turn_Rate ) 
+        wait(1)
+    robot.stop()
+    RLM.reset_angle(0)
 
 def Reset_gyro():
     Gyrogirl.reset_angle(0)
