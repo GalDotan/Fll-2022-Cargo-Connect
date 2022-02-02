321550b811c1ac90ab81aa1c1337fa04d8df8ece
GalDotan <gldotan@gmail.com>
2022-02-02 15:11:12 +0200

    testing

--- Green_run.py
+++ Green_run.py
@@ -1,4 +1,14 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
 from Object_creation import *
+from Driving_functions import *
+
 
 def Green_Arm_down():  
     RMM.run_target(-1000,1500 )
@@ -11,17 +21,17 @@ def Green_Arm_dwn():
 #Moving main arm almost down 
 def Green_run():
     Reset_gyro()
-    Reset_motores_angle(0)
+    RMM.reset_angle(0)
     ev3.speaker.beep()
     Green_Arm_dwn()
-    d_speed(200, 2, 0)
+    Gyroline_W_acceleration(200, 2, 0)
     Gyro_turn_left(-70)
     Gyro_turn_right(0)
-    d_speed(200, 1, 0)
+    Gyroline_W_acceleration(200, 1, 0)
     Gyro_turn_right(40)
-    d_speed(200, 1.1, 0)
+    Gyroline_W_acceleration(200, 1.1, 40)
     Gyro_turn_right(55)
-    d_speed(200, 0.14 ,55)
+    Gyroline_W_acceleration(200, 0.14 ,55)
     Green_Arm_up()
     robot.drive(250, 15)
     ev3.speaker.beep()
