c38756baaeb08f2c05a2af4d64a688a599468b9a
GalDotan <gldotan@gmail.com>
2022-02-02 15:58:24 +0200

    green run fix

--- Green_run.py
+++ Green_run.py
@@ -15,7 +15,7 @@ def Green_Arm_down():
 #Moving main arm down  
 def Green_Arm_up():
     RMM.run_target(-1000,0 )    
-#Moing the main arm up
+#Moving the main arm up
 def Green_Arm_dwn():
     RMM.run_target(-1000, 1420)
 #Moving main arm almost down 
@@ -24,15 +24,16 @@ def Green_run():
     RMM.reset_angle(0)
     ev3.speaker.beep()
     Green_Arm_dwn()
-    Gyroline_W_acceleration(200, 2, 0)
+    Gyroline_W_acceleration(200, 2.2, 0)
     Gyro_turn_left(-70)
     Gyro_turn_right(0)
     Gyroline_W_acceleration(200, 1, 0)
     Gyro_turn_right(40)
-    Gyroline_W_acceleration(200, 1.1, 40)
+    Gyroline_W_acceleration(200, 1.4, 40)
     Gyro_turn_right(55)
-    Gyroline_W_acceleration(200, 0.14 ,55)
+    Gyroline_W_acceleration(200, 0.31 ,55)
     Green_Arm_up()
-    robot.drive(250, 15)
     ev3.speaker.beep()
     Gyro_turn_left(-50)
+    robot.straight(90)
+    Gyro_turn_left(-30)
